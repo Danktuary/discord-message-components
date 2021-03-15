6d9eb496dd39a0bb8b0e0e0c07b0b60324a89cca
Sanctuary <sanc.pw@gmail.com>
2021-03-15 14:36:10 -0400

    feat(vue): Add DiscordMessage story

---
+++ packages/vue/src/components/DiscordMessage.stories.ts
@@ -0,0 +1,48 @@
+import { Meta, Story } from '@storybook/vue3'
+import DiscordMessage from './DiscordMessage.vue'
+import DiscordMessages from './DiscordMessages.vue'
+
+export default {
+	title: 'DiscordMessage',
+	component: DiscordMessage,
+	argTypes: {
+		roleColor: {
+			control: 'color',
+			defaultValue: '',
+		},
+	},
+} as Meta
+
+const Template: Story = args => ({
+	components: {
+		DiscordMessage,
+		DiscordMessages,
+	},
+	setup() {
+		return {
+			args,
+		}
+	},
+	template: `
+		<discord-messages>
+			<discord-message v-bind="args">Hello World</discord-message>
+		</discord-messages>
+	`,
+})
+
+export const Default = Template.bind({})
+
+export const Author = Template.bind({})
+Author.args = {
+	author: 'Author',
+}
+
+export const Bot = Template.bind({})
+Bot.args = {
+	bot: true,
+}
+
+export const RoleColor = Template.bind({})
+RoleColor.args = {
+	roleColor: '#0099ff',
+}
