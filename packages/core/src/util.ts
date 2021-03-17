18da033165a87c34b087da9fda1d969592d4d3c5
Sanctuary <sanc.pw@gmail.com>
2021-03-16 21:53:54 -0400

    feat(core): Add parseColorToRgba util function

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -1,3 +1,11 @@
+import colorToRgba from 'color-rgba'
+
+export const parseColorToRgba = (color?: string, alpha?: number): string | null => {
+	if (!color) return null
+	const [r, g, b, a] = colorToRgba(color) ?? []
+	return `rgba(${r},${g},${b},${alpha ?? a})`
+}
+
 export const parseTimestamp = (timestamp: Date | string): string => {
 	if (!(timestamp instanceof Date)) timestamp = new Date(timestamp)
 	const [month, day, year] = [timestamp.getMonth() + 1, timestamp.getDate(), timestamp.getFullYear()]
@@ -5,4 +13,3 @@ export const parseTimestamp = (timestamp: Date | string): string => {
 }
 
 export const defaultTimestamp = parseTimestamp(new Date())
-
