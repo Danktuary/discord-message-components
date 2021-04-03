bc4edb70b7e57ce702813eea38895676f38e98cb
Sanctuary <sanc.pw@gmail.com>
2021-04-03 19:28:47 -0400

    feat(react): Add authorIcon/Name/Url props to DiscordEmbed

--- packages/react/src/components/DiscordEmbed.tsx
+++ packages/react/src/components/DiscordEmbed.tsx
@@ -2,17 +2,33 @@ import React, { ReactElement, ReactNode } from 'react'
 import '@discord-message-components/core/dist/styles/discord-embed.css'
 
 export type DiscordEmbedProps = {
+	authorIcon?: string,
+	authorName?: string,
+	authorUrl?: string,
 	children?: ReactNode,
 	slot?: string,
 }
 
-export default function DiscordEmbed({ children }: DiscordEmbedProps): ReactElement {
+export default function DiscordEmbed({ authorIcon, authorName, authorUrl, children }: DiscordEmbedProps): ReactElement {
+	const content = {
+		author: (
+			<div className="discord-embed-author">
+				{authorIcon
+					&& <img src={authorIcon} alt="" className="discord-embed-author-icon" />}
+				{authorUrl
+					? <a href={authorUrl} target="_blank" rel="noopener noreferrer">{authorName}</a>
+					: <span>{authorName}</span>}
+			</div>
+		),
+	}
+
 	return (
 		<div className="discord-embed">
 			<div className="discord-embed-left-border"></div>
 			<div className="discord-embed-container">
 				<div className="discord-embed-content">
 					<div>
+						{authorName && content.author}
 						<div className="discord-embed-description">
 							{children}
 						</div>
