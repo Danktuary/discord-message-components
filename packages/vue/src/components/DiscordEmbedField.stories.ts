5b7cb4983e4c1e9d280acec1e56ac46d21ef8826
Sanctuary <sanc.pw@gmail.com>
2021-04-03 22:18:19 -0400

    feat(vue): Add DiscordEmbedField component

---
+++ packages/vue/src/components/DiscordEmbedField.stories.ts
@@ -0,0 +1,56 @@
+import { Meta, Story } from '@storybook/vue3'
+import DiscordEmbed from './DiscordEmbed.vue'
+import DiscordEmbedField from './DiscordEmbedField.vue'
+import DiscordEmbedFields from './DiscordEmbedFields.vue'
+import DiscordMessage from './DiscordMessage.vue'
+import DiscordMessages from './DiscordMessages.vue'
+
+export default {
+	title: 'DiscordEmbedField',
+	component: DiscordEmbedField,
+} as Meta
+
+const Template: Story = args => ({
+	components: {
+		DiscordEmbed,
+		DiscordEmbedField,
+		DiscordEmbedFields,
+		DiscordMessage,
+		DiscordMessages,
+	},
+	setup() {
+		return {
+			args,
+		}
+	},
+	template: `
+		<discord-messages>
+			<discord-message>
+				Hello World
+				<discord-embed slot="embeds">
+					Embed content
+					<discord-embed-fields slot="fields" v-if="args.title">
+						<discord-embed-field v-bind="args">{{ args.fieldValue }}</discord-embed-field>
+						<discord-embed-field v-bind="args">{{ args.fieldValue }}</discord-embed-field>
+						<discord-embed-field v-bind="args">{{ args.fieldValue }}</discord-embed-field>
+					</discord-embed-fields>
+				</discord-embed>
+			</discord-message>
+		</discord-messages>
+	`,
+})
+
+export const Default = Template.bind({})
+
+export const FieldWithTitle = Template.bind({})
+FieldWithTitle.args = {
+	title: 'Field title',
+	fieldValue: 'Field value',
+}
+
+export const InlineFieldWithTitle = Template.bind({})
+InlineFieldWithTitle.args = {
+	inline: true,
+	title: 'Field title',
+	fieldValue: 'Field value',
+}
---
+++ packages/vue/src/components/DiscordEmbedField.vue
@@ -0,0 +1,23 @@
+<template>
+	<div :class="{ 'discord-embed-field-inline': inline }" class="discord-embed-field">
+		<div class="discord-embed-field-title">{{ title }}</div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'DiscordEmbedField',
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+		inline: Boolean,
+	},
+})
+</script>
+
+<style src="@discord-message-components/core/dist/styles/discord-embed-field.css"></style>
