29dd0a9f73951c4a9dc6fefcb7bedbf08156a0fa
Sanctuary <sanc.pw@gmail.com>
2021-04-14 22:48:21 -0400

    feat(vue): Install plugin in Storybook preview

--- packages/vue/.storybook/preview.js
+++ packages/vue/.storybook/preview.js
@@ -1,3 +1,22 @@
+import { app } from '@storybook/vue3'
+import DiscordMessageComponents from '../src/index'
+
+app.use(DiscordMessageComponents, {
+	profiles: {
+		sanc: {
+			author: 'Sanc',
+			avatar: 'https://i.imgur.com/0TeacfY.png',
+			roleColor: '#0099ff',
+		},
+		bot: {
+			author: 'Bot',
+			avatar: 'green',
+			roleColor: '#3eaf7c',
+			bot: true,
+		},
+	},
+})
+
 export const parameters = {
 	actions: {
 		argTypesRegex: '^on[A-Z].*',
