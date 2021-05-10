bfbf334945d365cfe6d4622553daab9e31b077ec
Sanctuary <sanc.pw@gmail.com>
2021-05-09 20:53:21 -0400

    refactor(core): Add formatting option to parseTimestamp

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -10,8 +10,23 @@ export const parseColorToRgba = (color?: string, alpha?: number): string | null 
 	return `rgba(${r},${g},${b},${alpha ?? a})`
 }
 
-export const parseTimestamp = (timestamp: Date | string): string => {
+export type TimestampOptions = {
+	timestamp: Date | string
+	format: 'compact' | 'cozy'
+}
+
+export const parseTimestamp = ({ timestamp, format = 'cozy' }: TimestampOptions): string => {
 	if (!(timestamp instanceof Date)) timestamp = new Date(timestamp)
+
+	if (format === 'compact') {
+		const [hour, minutes] = [timestamp.getHours(), timestamp.getMinutes()]
+		return [
+			hour > 12 ? hour - 12 : (hour === 0 ? 12 : hour),
+			`:${minutes.toString().padStart(2, '0')} `,
+			hour >= 12 ? 'PM' : 'AM',
+		].join('')
+	}
+
 	const [month, day, year] = [timestamp.getMonth() + 1, timestamp.getDate(), timestamp.getFullYear()]
 	return `${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}/${year}`
 }
