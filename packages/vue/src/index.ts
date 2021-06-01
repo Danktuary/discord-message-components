d668cae3691d1b111b7f059ae0795497b0c50493
Sanctuary <sanc.pw@gmail.com>
2021-06-01 00:06:16 -0400

    fix(vue): Export all components

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -31,7 +31,7 @@ const defaultOptions = {
 	profiles: {},
 }
 
-export default function install(app: App, options: DiscordMessageOptions): void {
+export const install = (app: App, options: DiscordMessageOptions): void => {
 	const avatars = { ...defaultOptions?.avatars, ...options?.avatars }
 
 	app.config.globalProperties.$discordOptions = {
@@ -44,9 +44,15 @@ export default function install(app: App, options: DiscordMessageOptions): void 
 	} as DiscordMessageOptions
 }
 
+export { default as DiscordButton } from './components/DiscordButton.vue'
+export { default as DiscordButtons } from './components/DiscordButtons.vue'
 export { default as DiscordEmbed } from './components/DiscordEmbed.vue'
 export { default as DiscordEmbedField } from './components/DiscordEmbedField.vue'
 export { default as DiscordEmbedFields } from './components/DiscordEmbedFields.vue'
+export { default as DiscordInteraction } from './components/DiscordInteraction.vue'
+export { default as DiscordMarkdown } from './components/DiscordMarkdown.vue'
 export { default as DiscordMention } from './components/DiscordMention.vue'
 export { default as DiscordMessage } from './components/DiscordMessage.vue'
 export { default as DiscordMessages } from './components/DiscordMessages.vue'
+export { default as DiscordReaction } from './components/DiscordReaction.vue'
+export { default as DiscordReactions } from './components/DiscordReactions.vue'
